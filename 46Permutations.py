d235cfe03a551463226b99659de4af214bc7a8f7
LiliMeng <lilimeng1103@gmail.com>
2018-05-11 09:42:20 -0700

    Create 46Permutations.py

---
+++ 46Permutations.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        self.dfs(nums, [], res)
+        return res
+    
+    def dfs(self, nums, path, res):
+        if not nums:
+            res.append(path)
+            # return # backtracking
+        for i in xrange(len(nums)):
+            self.dfs(nums[:i]+nums[i+1:], path+[nums[i]], res)
